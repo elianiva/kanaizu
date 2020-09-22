561c5fc338c4faed33563244228b470b1dafd235
elianiva <dicha.arkana03@gmail.com>
2020-09-22 23:46:11 +0700

    feat: added button component

---
+++ src/pages/_components/Button.svelte
@@ -0,0 +1,10 @@
+<a
+  class="py-4 px-8 font-4xl text-2xl rounded-md shadow-md smooth {styles}"
+  href={$url(to)}>
+  {txt}
+</a>
+
+<script>
+  import { url } from "@sveltech/routify"
+  export let to, txt, styles
+</script>
