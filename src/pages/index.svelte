99020167ab368c15e6d3c6320bfee0898fa8dd65
elianiva <dicha.arkana03@gmail.com>
2020-09-22 23:46:46 +0700

    feat: finished homepage

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,25 @@
-<script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@sveltech/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
-</script>
+<div class="w-full flex flex-col items-center justify-center">
+  <h1
+    class="text-8xl font-bold mplus tracking-widest px-8 py-4 bg-gray-900
+      text-white rounded-lg mt-4xl mt-far">
+    クナイズ <span class="text-gray-900 bg-gray-100 px-4 rounded-lg">kunaizu</span>
+  </h1>
+  <span class="text-4xl mplus my-8">ひらがな and カタカナ quiz app</span>
+  <div class="flex gap-4">
+    <Button
+      to="/hiragana"
+      txt="Hiragana"
+      styles="montserrat font-semibold hover:text-blue-600 hover:move-up" />
+    <Button
+      to="/katakana"
+      txt="Katakana"
+      styles="montserrat font-semibold hover:text-blue-600 hover:move-up" />
+  </div>
+</div>
 
-<RoutifyIntro />
+<script>
+  import { metatags } from "@sveltech/routify"
+  import Button from "./_components/Button.svelte"
+  metatags.title = "Kunaizu | クナイズ"
+  metatags.description = "Hiragana and katakana quiz app"
+</script>
