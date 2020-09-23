668150095ee3ff172c917bb2054ff8367f0f8a85
elianiva <dicha.arkana03@gmail.com>
2020-09-23 16:46:16 +0700

    feat: added option component

---
+++ src/pages/_components/Option.svelte
@@ -0,0 +1,15 @@
+<div class="flex items-center justify-between gap-8">
+  <label class="text-xl" for="level">{data.title}</label>
+  <select
+    class="py-2 px-4 bg-white rounded-md shadow-md text-lg"
+    name="level"
+    id="level">
+    {#each data.items as item}
+      <option value={item.value}>{item.text}</option>
+    {/each}
+  </select>
+</div>
+
+<script>
+  export let data
+</script>
