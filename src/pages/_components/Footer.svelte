8bd29b5eee5573d4618b514eeefc139d48ff4d2f
elianiva <dicha.arkana03@gmail.com>
2020-09-23 12:52:20 +0700

    feat: moved Footer to its own component

---
+++ src/pages/_components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="text-center p-4 border-t-2 mt-16 border-gray-200 montserrat">
+  <span class="block">
+    Powered by <a
+      class="font-bold text-orange-500"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="norel noreferrer">Svelte</a> and hosted on <a class="font-bold
+        text-blue-600" href="https://vercel.app" target="_blank" rel="norel noreferrer">Vercel</a>
+  </span>
+  <span class="block">
+    Source code available on <a class="font-bold text-purple-600" href="https://github.com" target="_blank" rel="norel noreferrer">Github</a>
+  </span>
+</footer>
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,16 +1,1 @@
 <slot />
-<footer
-  class="text-center absolute left-0 bottom-0 right-0 p-4 border-t-2
-    border-gray-200 montserrat">
-  <span class="block">
-    Powered by <a
-      class="font-bold text-orange-500"
-      href="https://svelte.dev"
-      target="_blank"
-      rel="norel noreferrer">Svelte</a> and hosted on <a class="font-bold
-        text-blue-600" href="https://vercel.app" target="_blank" rel="norel noreferrer">Vercel</a>
-  </span>
-  <span class="block">
-    Source code available on <a class="font-bold text-purple-600" href="https://github.com" target="_blank" rel="norel noreferrer">Github</a>
-  </span>
-</footer>
