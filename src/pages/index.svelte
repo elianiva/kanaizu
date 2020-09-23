803661b1ffd2d20da1155951a59b917c1c0c3f57
elianiva <dicha.arkana03@gmail.com>
2020-09-23 20:50:55 +0700

    refactor: moved data from index to store

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -27,51 +27,10 @@
   import Button from "./_components/Button.svelte"
   import Footer from "./_components/Footer.svelte"
   import Modal from "./_components/Modal.svelte"
-  import { isVisible } from "./_components/stores.js"
+  import { isVisible, data } from "./_components/stores.js"
 
   let isModalShown
 
-  isVisible.subscribe(value => {
-    isModalShown = value
-  })
-
-  const data = {
-    level: {
-      title: "JLPT Level",
-      items: [
-        { value: "n5", text: "N5" },
-        { value: "n4", text: "N4" },
-        { value: "n3", text: "N3" },
-        { value: "n2", text: "N2" },
-        { value: "n1", text: "N1" },
-      ],
-    },
-    limit: {
-      title: "Word Limit",
-      min: 10,
-      max: 800,
-      placeholder: 40
-    },
-    time: {
-      title: "Time Limit",
-      items: [
-        { value: "10s", text: "10 secs" },
-        { value: "30s", text: "30 secs" },
-        { value: "1m", text: "1 min" },
-        { value: "2m", text: "2 mins" },
-        { value: "5m", text: "5 mins" },
-      ],
-    },
-    font: {
-      title: "Font Family",
-      items: [
-        { value: "noto-sans", text: "Noto Sans JP" },
-        { value: "noto-serif", text: "Noto Serif JP" },
-        { value: "mplus-normal", text: "M PLUS 1p" },
-        { value: "mplus", text: "M PLUS Rounded 1c" },
-      ],
-    },
-  }
 
   metatags.title = "Kanaizu | カナイズ"
   metatags.description = "Hiragana and katakana quiz app"
