5f1eee7bd04c7d7daadb6f6b81bc69ae5a7872c9
elianiva <dicha.arkana03@gmail.com>
2020-09-24 22:50:11 +0700

    feat: added Counter component

---
+++ src/pages/hiragana/_components/Counter.svelte
@@ -0,0 +1,11 @@
+<div
+  class="{type === 'countdown' ? 'left-4' : 'right-4'} fixed top-4 py-4 px-8 bg-gray-800
+    rounded-lg">
+  <span
+    class="text-2xl text-white mplus font-bold">{Array.isArray(value) ? `${value[0]} / ${value[1]}` : value}</span>
+</div>
+
+<script>
+  export let type
+  export let value
+</script>
