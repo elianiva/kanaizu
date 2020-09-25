da85f7b36894c78ff4e96037b336028c5bd4e4e4
elianiva <dicha.arkana03@gmail.com>
2020-09-25 14:15:01 +0700

    feat: button hover effect

--- src/pages/result/index.svelte
+++ src/pages/result/index.svelte
@@ -11,10 +11,11 @@
       link={true}
       href="/"
       txt="Play Again"
-      styles="bg-gray-900 text-white mplus px-4 py-2 self-start" />
+      styles="bg-gray-900 text-white mplus px-4 py-2 self-start hover:bg-black hover:move-up" />
   </div>
   <div class="flex flex-col items-center w-full p-4 bg-white rounded-md shadow-md mt-8">
     <span class="text-4xl font-bold mplus">Details</span>
+    <hr class="w-1/12 h-1 bg-gray-300 rounded-md">
     <div class="w-full grid grid-cols-2 gap-8 mt-4">
       <Table data={$scores.correct} title="Correct" type="correct"/>
       <Table data={$scores.wrong} title="Wrong" type="wrong"/>
